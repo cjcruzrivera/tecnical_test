ee1b6d1cf89fe362a709cbd676af8f3a87d0dcf3
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2025-01-25 17:51:24 +0100

    implement register page

--- tecnical_test_front/src/App.js
+++ tecnical_test_front/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/" element={<Navigate to="/login" />} />
           </Routes>
         </div>
